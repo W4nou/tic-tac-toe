1cf7b05d28be7858f2ec198f09a4a460d15fa3dc
W4nou <wanou35.foulon@laposte.net>
2024-05-23 21:42:56 +0200

    Player refactor

--- src/main/java/ihm_project/tictactoe/Player.java
+++ src/main/java/ihm_project/tictactoe/Player.java
@@ -4,6 +4,7 @@ import javafx.scene.effect.Effect;
 import javafx.scene.image.Image;
 
 public class Player {
+
     public static final String P1_NAME_DEFAULT = "Joueur 1";
     public static final String P2_NAME_DEFAULT = "Joueur 2";
     public static final String BOT_NAME_DEFAULT = "Ordinateur";
@@ -13,34 +14,34 @@ public class Player {
     private String name;
     private boolean bot;
 
-    public Player(Image shape, Effect color, String name) {
+    protected Player(Image shape, Effect color, String name) {
         this.shape = shape;
         this.color = color;
         this.name = name;
         this.bot = false;
     }
 
-    public void setBot(boolean isBot) {
+    protected void setBot(boolean isBot) {
         bot = isBot;
     }
 
-    public boolean isBot() {
+    protected boolean isBot() {
         return bot;
     }
 
-    public Image getShape() {
+    protected Image getShape() {
         return shape;
     }
 
-    public Effect getColor() {
+    protected Effect getColor() {
         return color;
     }
 
-    public String getName() {
+    protected String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    protected void setName(String name) {
         this.name = name;
     }
 }
